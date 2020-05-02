163a3edfede202c8150464b8579a4bdbbde9abe0
scoobis <gabriel.bjorlin@gmail.com>
2020-05-02 18:01:14 +0200

    Setup and test

---
+++ project/src/Main.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		System.out.println("woooow!");
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
