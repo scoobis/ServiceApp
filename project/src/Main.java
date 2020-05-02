1fea20c159d4bc342e52e2c1c2f0f34a753da5e9
scoobis <gabriel.bjorlin@gmail.com>
2020-05-02 18:13:03 +0200

    Database, and basic javaFX setup

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,15 +1,33 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import model.database.CreateTables;
 
 public class Main extends Application {
 	
+	Stage window;
+	
 	public static void main(String[] args) {
-		System.out.println("woooow!");
+		
+		// Create new tables (if it does not already exists)
+		CreateTables newTables = new CreateTables();
+		newTables.executeCreateAllTables();
+		
+		launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		
+		StackPane layout = new StackPane();
+		Scene scene = new Scene(layout, 1800, 850);
+		
+		window.setScene(scene);
+	    window.setMaximized(true);
+	    window.setTitle("Service Application");
+	    window.show();
 		
 	}
 }
---
+++ project/src/model/database/CreateTables.java
@@ -0,0 +1,131 @@
+package model.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class CreateTables {
+Connection connection = null;
+	
+	public CreateTables() {
+		connection = DatabaseConnector.getConnection();
+	}
+	
+	public void executeCreateAllTables() {
+		this.createSuperAdmin();
+		this.createAdmin();
+		this.createUser();
+		this.createShop();
+		this.createCompany();
+		this.createOrder();
+		this.createService();
+		this.createCustomer();
+		System.out.println("All create functions executed!");
+	}
+	
+	private void createSuperAdmin() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS super_admin"
+					+ "(id int, "
+					+ "company_name varchar(25), "
+					+ "phone varchar(25), "
+					+ "email varchar(155), "
+					+ "password varchar(512), "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createAdmin() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS admin"
+					+ "(id int, "
+					+ "company_name varchar(25), "
+					+ "shop_id int, "
+					+ "phone varchar(25), "
+					+ "email varchar(155), "
+					+ "password varchar(512), "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createUser() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS user"
+					+ "(id int, "
+					+ "company_name varchar(25), "
+					+ "shop_id int, "
+					+ "phone varchar(25), "
+					+ "email varchar(155), "
+					+ "password varchar(512), "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createShop() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS shop"
+					+ "(id int, "
+					+ "company_name varchar(25), "
+					+ "adress varchar(128), "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createCompany() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS company"
+					+ "(name varchar(64), "
+					+ "adress varchar(128), "
+					+ "PRIMARY KEY(name))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createOrder() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS customer_order"
+					+ "(id int, "
+					+ "service_id int, "
+					+ "customer_id int, "
+					+ "shop_id int, "
+					+ "company_name varchar(64), "
+					+ "price int, "
+					+ "completed boolean, "
+					+ "date DATE, "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createService() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS service"
+					+ "(id int, "
+					+ "company_name varchar(64), "
+					+ "title varchar(64), "
+					+ "description TEXT, "
+					+ "price int, "
+					+ "uniques boolean, "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+	
+	private void createCustomer() {
+		try {
+			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS customer"
+					+ "(id int, "
+					+ "phone varchar(25), "
+					+ "email varchar(155), "
+					+ "name varchar(128), "
+					+ "adress varchar(128), "
+					+ "status varchar(64), "
+					+ "company_name varchar(64), "
+					+ "PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e) { System.out.println(e); }
+	}
+}
