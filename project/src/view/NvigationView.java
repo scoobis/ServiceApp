af1c19fd9b44d3f1f98c386da6703ccbad7423fb
scoobis <gabriel.bjorlin@gmail.com>
2020-05-04 13:04:29 +0200

    updated

---
+++ project/src/view/NvigationView.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class NvigationView extends Application implements IView {
+
+	@Override
+	public void render(Stage primaryStage) {
+		
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
