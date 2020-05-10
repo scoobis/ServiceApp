49cde44567d53b17cd7b9f75034bb48e1d71a5c9
je223gs <je223gs@student.lnu.se>
2020-05-10 17:47:36 +0200

    Finding some problems in Database

--- project/src/model/database/tests/CustomerDatabaseUnitTests.java
+++ project/src/model/database/tests/CustomerDatabaseUnitTests.java
@@ -7,9 +7,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
@@ -23,8 +21,9 @@ class CustomerDatabaseUnitTests {
 	Customer c;
 	Customer b;
 	
-	public void init() {
-		// Use Mockito here
+	public void init() throws SQLException {
+		
+		db.reset();
 		c = new Customer();
 		c.setActive(true);
 		c.setAddress("Street 1");
@@ -52,13 +51,70 @@ class CustomerDatabaseUnitTests {
 	}
 	
 	@Test
-	void saveCustomerFailure() throws InvalidKeySpecException, SQLException {
+	void saveCustomerFailureDueToDuplicateUser() throws InvalidKeySpecException, SQLException {
 		this.init();
 		
 		assertThrows(MySQLIntegrityConstraintViolationException.class, () -> {
 			db.saveCustomer(c);
 			db.saveCustomer(c);
 		});
+		
+		db.reset();
+	}
+	
+	@Test
+	void testDeleteCustomer() throws InvalidKeySpecException, SQLException {
+		this.init();
+		
+		db.saveCustomer(c);
+		assertEquals(1, db.deleteCustomer("1"));
+		db.reset();
+	}
+	
+	
+	@Test
+	void testEditCustomerSuccess() throws NoSuchAlgorithmException, InvalidKeySpecException, SQLException {
+		this.init();
+		
+		db.saveCustomer(c);
+		db.editCustomer("1", b);
+		
+		assertEquals("JOHN", db.getCustomerById("1").getName());
+		
+		db.reset();
+	}
+	
+	/**
+	 * TODO: Its hard to test edit becuase it doesn't throw any Exception when entering a invalid ID.
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 * @throws SQLException
+	 */
+	@Test
+	void testEditCustomerFailureDueToInvalidId() throws NoSuchAlgorithmException, InvalidKeySpecException, SQLException {
+		this.init();
+
+		db.saveCustomer(c);
+
+		assertThrows(InvalidKeySpecException.class, () -> {
+			db.editCustomer("1", c); // Invalid ID 2.
+		});
+		
+		db.reset();
+	
+	}
+	
+	@Test
+	void testGetAllCustomers() throws InvalidKeySpecException, SQLException {
+		this.init();
+		
+		db.saveCustomer(c);
+		db.saveCustomer(b);
+	
+		ArrayList<Customer> testArr = db.getAllCustomers();
+		
+		assertEquals(c.getName(), testArr.get(0).getName());
+		assertEquals(b.getName(), testArr.get(1).getName());
 		
 		db.reset();
 	}
